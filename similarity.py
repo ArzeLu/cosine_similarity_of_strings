84ad9802c419ea9d7d302cce4d4f613cdcc11d63
Arze Lu <lu_arze@outlook.com>
2025-02-12 23:53:23 -0800

    commit

---
+++ similarity.py
@@ -0,0 +1,71 @@
+# -------------------------------------------------------------------------
+# AUTHOR: Arze Lu
+# FILENAME: similarity.py
+# SPECIFICATION: Find similarities of documents
+# FOR: CS 5990 (Advanced Data Mining) - Assignment #1
+# TIME SPENT: how long it took you to complete the assignment
+# -----------------------------------------------------------*/
+
+#IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy,
+#pandas, or other sklearn modules.
+#You have to work here only with standard dictionaries, lists, and arrays
+
+# Importing some Python libraries
+import csv
+from sklearn.metrics.pairwise import cosine_similarity
+
+documents = []
+
+#reading the documents in a csv file
+with open('cleaned_documents.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    for i, row in enumerate(reader):
+        if i > 0: #skipping the header
+            documents.append(row)
+            #print(row)
+
+#Building the document-term matrix by using binary encoding.
+#You must identify each distinct word in the collection without applying any transformations, using
+# the spaces as your character delimiter.
+#--> add your Python code here
+
+for document in documents:
+    document = document[1].split(" ")
+    #print(document)
+
+total_docs = len(documents)
+
+docTermMatrix = []
+
+# Compare the pairwise cosine similarities and store the highest one
+# Use cosine_similarity([X], [Y]) to calculate the similarities between 2 vectors
+# --> Add your Python code here
+
+# initializing the matrix
+for i in range(0, total_docs):
+    row = [[0] * total_docs]
+    docTermMatrix.append(row)
+
+for i in range(0, total_docs):
+    for j in range(i, total_docs):
+        words = list(set(documents[i]).union(set(document[j])))
+        vector_1 = []
+        vector_2 = []
+
+        for word in words:
+            if word in documents[i]:
+                vector_1.append(1)
+            else:
+                vector_1.append(0)
+
+        for word in words:
+            if word in documents[j]:
+                vector_2.append(1)
+            else:
+                vector_2.append(0)
+
+        print(cosine_similarity([vector_1, vector_2]))
+
+# Print the highest cosine similarity following the information below
+# The most similar documents are document 10 and document 100 with cosine similarity = x
+# --> Add your Python code here
